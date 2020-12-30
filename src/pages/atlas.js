197c14cc97bb6dee328e989dee30ba13968c5567
mmalone30 <mitch@mitchmaloneonline.com>
2020-12-29 17:14:17 -0800

    content!

--- src/pages/atlas.js
+++ src/pages/atlas.js
@@ -119,6 +119,65 @@ export default () => {
           Some photos of me in the field conducting research. My approach to research is pretty simple—expose myself to users and really listen and watch what they are doing.
         </figcaption>
       </figure>
+      <section>
+        <h3>Design Principles</h3>
+        <p>From the research and analysis, my team and I developed design principles that would guide our decision-making. These principles were most useful in design critiques and helped frame the discussion better. If we had good responses to these questions, then we knew we were on the right track.</p>
+        <div class="panels two">
+          <div class="panel">
+            <h4>Coherence</h4>
+            <ul>
+              <li>Do the parts form a unified and understandable whole?</li>
+              <li>Do the parts align with user goals in a clear and logical way?</li>
+              <li>Are the seams in the experience clear, consistent, and logical?</li>
+            </ul>
+          </div>
+          <div class="panel">
+            <h4>Intentionality</h4>
+            <ul>
+              <li>Are the relevant parts presented at the right time?</li>
+              <li>Do the parts enable a specific action under a given circumstance?</li>
+              <li>Are the conditions of the parts clearly presented?</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section>
+        <h3>Success Metrics &amp; Current Values</h3>
+        <p>
+          With the problem and design principles well understood, we defined what success looks like for this project.
+        </p>
+        <table>
+          <thead>
+            <tr>
+              <td>Success</td>
+              <td>Task</td>
+              <td>Average Time (in sec)</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Reduce track and trace time per shipment per user</td>
+              <td>For any 5 shipments in the Shipments list, tell me the current location, next location (if any), and its job status</td>
+              <td>194</td>
+            </tr>
+            <tr>
+              <td>Reduce time to make appointment per shipment per user</td>
+              <td>Find 2 shipments that are available to outgate and enter this outgate appointment: 00:00 00/00/00</td>
+              <td>92</td>
+            </tr>
+            <tr>
+              <td>Reduce mark empty time</td>
+              <td>Find 2 shipments to mark empty</td>
+              <td>51</td>
+            </tr>
+            <tr>
+              <td>Reduce mark availability time</td>
+              <td>Find 2 shipments to mark available</td>
+              <td>61</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </div>
   </div>
   </div>
